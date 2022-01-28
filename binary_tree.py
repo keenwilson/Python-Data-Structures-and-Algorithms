d1a5208b070d6093b625704bd083a760792745b5
Keen <keencwilson@gmail.com>
2022-01-27 19:33:04 -0800

    Binary Tree: calculate the height of nodes

--- binary_tree.py
+++ binary_tree.py
@@ -57,13 +57,81 @@ class Tree:
                 return True
         return False
 
+    def traversePreOrder(self):
+        self.__traversePreOrder(self.root)
+
+    def __traversePreOrder(self, root):
+        # Base condition to avoid the cycle
+        if not root:
+            return
+
+        # Depth-First, Pre-order
+        # Root, left, Right
+        print(root.value)
+        self.__traversePreOrder(root.leftChild)
+        self.__traversePreOrder(root.rightChild)
+
+    def traverseInOrder(self):
+        self.__traverseInOrder(self.root)
+
+    def __traverseInOrder(self, root):
+        # Base condition to avoid the cycle
+        if not root:
+            return
+
+        # Depth-First, In-order
+        # Left, Root, Right
+        self.__traverseInOrder(root.leftChild)
+        print(root.value)
+        self.__traverseInOrder(root.rightChild)
+
+    def traversePostOrder(self):
+        self.__traversePostOrder(self.root)
+
+    def __traversePostOrder(self, root):
+        # Base condition to avoid the cycle
+        if not root:
+            return
+
+        # Depth-First, Post-order
+        # Left, Right, Root
+        self.__traversePostOrder(root.leftChild)
+        self.__traversePostOrder(root.rightChild)
+        print(root.value)
+
+    # Depth
+    # Calculate the depth by counting the number of edges
+    # from root to target node
+
+    def height(self):
+        return self.__height(self.root)
+
+    # Height
+    # Find the longest path from the leaf node to the root node
+    # The height of the tree = the height of the root node
+    # Calculate the height of the left and right subtrees and add 1
+    # 1 + max(height(L),height(R))
+    # Post order traversal - visit the leaves first and pass the value up until we get the root node
+    def __height(self, root):
+        if not root:
+            return -1
+        # Base Condition
+        if not root.leftChild and not root.rightChild:
+            # The leaf node has 0
+            return 0
+        return 1 + max(self.__height(root.leftChild), self.__height(root.rightChild))
+
 
 tree = Tree()
-tree.insert(7)
-tree.insert(4)
-tree.insert(9)
-tree.insert(1)
-tree.insert(6)
-tree.insert(8)
-tree.insert(10)
-print(tree.find(11))
+# tree.insert(7)
+# tree.insert(4)
+# tree.insert(9)
+# tree.insert(1)
+# tree.insert(6)
+# tree.insert(8)
+# tree.insert(10)
+# print(tree.find(11))
+# print(tree.traverseInOrder())
+# print(tree.traversePreOrder())
+# print(tree.traversePostOrder())
+print(tree.height())
